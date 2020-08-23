f4a3b7ee9943085ebb9d72c5de2b1f84cfcece77
Lakshmi N Prasad Kakumani <kln550@gmail.com>
2020-08-23 14:41:22 -0400

    DEV: Simple Scala unit tests.

---
+++ README.md
@@ -0,0 +1,8 @@
+# scalatest-example
+
+Simple introduction to the scalatest library.
+
+## Running tests
+
+Run `sbt test` to run the entire test suite.
+sbt 
---
+++ src/main/scala/com/scalatest/example/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.scalatest.example;
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
+object HelloWorld {
+
+  def withGreeting()(df: DataFrame): DataFrame = {
+    df.withColumn("greeting", lit("hello world"))
+  }
+
+}
