20e08500818e56d277638bc8554cdacec75626de
Lakshmi N Prasad Kakumani <kln550@gmail.com>
2020-09-03 09:58:45 -0400

    Added test case for reading parque file.

--- src/test/scala/com/scalatest/example/S3FileVerrifyTest.scala
+++ src/test/scala/com/scalatest/example/S3FileVerrifyTest.scala
@@ -5,13 +5,10 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
 class S3FileVerrifyTest extends FunSuite with BeforeAndAfter {
-  // Access Key ID: AKIAIKM2JQLJTJHLEQEA
-  // Secret Access Key: 8LeP/ZIoQtgMUWn+mbsk9yfIy701eyF1J3Aq4wfw
-
   /* These Keys would be available to you in  "Security Credentials" of
     your Amazon S3 account */
   val AWS_ACCESS_KEY = "**********"
-  val AWS_SECRET_KEY = "8LeP/ZIoQtgMUWn+mbsk9yfIy701eyF1J3Aq4wfw"
+  val AWS_SECRET_KEY = "*********"
   val bucketName = "prasadvatest"
   val fileName = "spark_scala_doc2.pdf"
   val provider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY))
