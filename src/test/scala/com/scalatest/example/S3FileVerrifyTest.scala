4c2200284b5e746fd4bf629d950d336ffc4dcf45
Lakshmi N Prasad Kakumani <kln550@gmail.com>
2020-09-03 23:44:01 -0400

    Added test case for reading parque file.

--- src/test/scala/com/scalatest/example/S3FileVerrifyTest.scala
+++ src/test/scala/com/scalatest/example/S3FileVerrifyTest.scala
@@ -2,19 +2,28 @@ package com.scalatest.example
 
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap, FunSuite}
 
-class S3FileVerrifyTest extends FunSuite with BeforeAndAfter {
+class S3FileVerrifyTest extends FunSuite with BeforeAndAfterAllConfigMap {
   /* These Keys would be available to you in  "Security Credentials" of
     your Amazon S3 account */
-  val AWS_ACCESS_KEY = "**********"
-  val AWS_SECRET_KEY = "*********"
-  val bucketName = "prasadvatest"
-  val fileName = "spark_scala_doc2.pdf"
+  val AWS_ACCESS_KEY = ""
+  val AWS_SECRET_KEY = ""
+  var bucketName = ""
+  var fileName = ""
   val provider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY))
   val amazonS3Client = AmazonS3ClientBuilder.standard().withCredentials(provider).withRegion("us-east-1").build()
 
+  override def beforeAll(configMap: ConfigMap) = {
+    bucketName = configMap.get("bucketName").fold("")(_.toString)
+    fileName = configMap.get("fileName").fold("")(_.toString)
+    println("bucketName=" + bucketName)
+    println("fileName=" + fileName)
+  }
+
+/*
   test("Verify S3 file exist") {
+
       // This will create a bucket for storage
       val isExist = amazonS3Client.doesObjectExist(bucketName, fileName)
       if (isExist) {
@@ -23,4 +32,29 @@ class S3FileVerrifyTest extends FunSuite with BeforeAndAfter {
         assert(false)
       }
     }
+*/
+
+
+  test("Verify S3 bucket file count exist") {
+    // This will create a bucket for storage
+    val count = countObjects(bucketName)
+     assert(count==1)
+  }
+
+
+  def countObjects(bucketName: String): Int = {
+    var count = 0
+    var objectListing = amazonS3Client.listObjects(bucketName)
+    var currentBatchCount = objectListing.getObjectSummaries.size
+    while ( {
+      currentBatchCount != 0
+    }) {
+      count += currentBatchCount
+      objectListing = amazonS3Client.listNextBatchOfObjects(objectListing)
+      currentBatchCount = objectListing.getObjectSummaries.size
+    }
+    count
+  }
+
+
   }
